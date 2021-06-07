01b52979eebc326cafce012f760931df4cec4711
Fulvio Corno <fulvio.corno@polito.it>
2021-06-07 09:24:04 +0200

    Punti 1b e 1c

--- src/main/java/it/polito/tdp/genes/db/GenesDao.java
+++ src/main/java/it/polito/tdp/genes/db/GenesDao.java
@@ -6,11 +6,41 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.genes.model.Genes;
+import it.polito.tdp.genes.model.Interactions;
 
 
 public class GenesDao {
+	
+	public List<Interactions> getInteractions(Map<String, Genes> genesIdMap) {
+		
+		String sql = "SELECT * FROM Interactions" ;
+		Connection conn = DBConnect.getConnection() ;
+		
+		List<Interactions> result = new ArrayList<>();
+		try {
+			PreparedStatement st = conn.prepareStatement(sql) ;
+			ResultSet res = st.executeQuery() ;
+			
+			while(res.next()) {
+				Genes gene1 = genesIdMap.get(res.getString("geneID1")) ;
+				Genes gene2 = genesIdMap.get(res.getString("geneID2")) ;
+				
+				if( gene1!=null && gene2!=null && !gene1.equals(gene2) ) {
+					result.add(new Interactions(gene1, gene2,
+							res.getString("type"), res.getDouble("Expression_Corr"))) ;
+				}
+			}
+			conn.close();
+			return result ;
+		} catch(SQLException ex) {
+			throw new RuntimeException("Database error", ex) ;
+		}
+		
+		
+	}
 	
 	public List<Genes> getAllEssentialGenes(){
 		String sql = "SELECT DISTINCT GeneID, Essential, Chromosome FROM genes WHERE Essential='Essential' ORDER BY GeneID";
--- src/main/java/it/polito/tdp/genes/model/Model.java
+++ src/main/java/it/polito/tdp/genes/model/Model.java
@@ -1,6 +1,8 @@
 package it.polito.tdp.genes.model;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -12,15 +14,31 @@ import it.polito.tdp.genes.db.GenesDao;
 public class Model {
 	
 	private List<Genes> essentialGenes ;
+	private Map<String, Genes> essentialGenesIdMap ;
+	
 	private Graph<Genes, DefaultWeightedEdge> grafo ;
 	
 	public String creaGrafo() {
 		GenesDao dao = new GenesDao() ;
 		this.essentialGenes = dao.getAllEssentialGenes() ;
+		this.essentialGenesIdMap = new HashMap<>() ;
+		for(Genes g: this.essentialGenes) 
+			this.essentialGenesIdMap.put(g.getGeneId(), g) ;
 		
 		this.grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class) ;
 		
 		Graphs.addAllVertices(this.grafo, this.essentialGenes) ;
+		
+		List<Interactions> archi = dao.getInteractions(essentialGenesIdMap) ;
+		for(Interactions arco: archi) {
+			if(arco.getGene1().getChromosome() == arco.getGene2().getChromosome() ) {
+				Graphs.addEdge(this.grafo, arco.getGene1(), arco.getGene2(), 
+						Math.abs(arco.getExpressionCorr()*2.0)) ;
+			} else {
+				Graphs.addEdge(this.grafo, arco.getGene1(), arco.getGene2(),
+						Math.abs(arco.getExpressionCorr())) ;
+			}
+		}
 		
 		return String.format("Grafo creato con %d vertici e %d archi\n",
 				this.grafo.vertexSet().size(), this.grafo.edgeSet().size()) ;
